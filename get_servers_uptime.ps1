db4eec90d7f125a29e6155c82dab05aa254e0808
SteelRain <AnsRomanov@gmail.com>
2014-07-02 14:29:42 +0400

    Update get_servers_uptime.ps1

--- get_servers_uptime.ps1
+++ get_servers_uptime.ps1
@@ -1,4 +1,22 @@
+<#
+    Script gets uptime data for Windows & Linux servers. Use SSH-Sessions
+    module to implement SSH connections through PowerShell.
+    
+    Copyright (C) 2014 Andrey Romanov
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#>
 
 # Get windows servers uptime part
 Set-ExecutionPolicy -Scope CurrentUser Unrestricted
